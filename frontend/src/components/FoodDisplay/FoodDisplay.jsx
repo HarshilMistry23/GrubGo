923c3938514837da9ec20ec256c748521c91c00a
HarshilMistry23 <harshilmistry23@gmail.com>
2024-09-13 20:38:12 +0530

    Spinner added and title changed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,13 +8,20 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
+        {food_list.length === 0 ? (
+         <div className="spinner-container">
+         <div className="spinner" />
+         </div>
+      ) : (
         <div className='food-display-list'>
+          {console.log(food_list)}
             {food_list.map((item,index)=>{
                 if(category=== "All" || category === item.category){
                   return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
                 }
             })}
         </div>
+      )}
     </div>
   )
 }
